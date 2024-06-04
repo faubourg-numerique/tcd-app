6270a13dd78245026caebf1bfc7067f6e661f212
Mattthieu Marin <pvcsam51@gmail.com>
2024-06-04 17:48:38 +0200

    add connexion

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Tdc_dashboardView from '../views/Tdc_dashboardView.vue'
+import OauthCallback from '@/components/OauthCallback.vue'
+import { useOauthStore } from '../stores/oauth-store'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,9 +10,30 @@ const router = createRouter({
       path: '/',
 
       name: 'Dashboard',
-      component: Tdc_dashboardView 
+      component: Tdc_dashboardView
     },
+    {
+      path: '/oauth2/callback',
+      name: 'callback',
+      component: OauthCallback
+    }
   ]
+})
+
+router.beforeEach((to) => {
+  const oauthStore = useOauthStore()
+
+  if (!oauthStore.isConnected && to.name != 'callback') {
+    const linkRedirect =
+      import.meta.env.VITE_IDENTITY_MANAGER_URL +
+      '/oauth2/authorize?client_id=' +
+      import.meta.env.VITE_IDENTITY_MANAGER_APP_CLIENT_ID +
+      '&response_type=token&redirect_uri=' +
+      import.meta.env.VITE_IDENTITY_MANAGER_APP_REDIRECT_URI +
+      '&state=false'
+    window.location.href = linkRedirect
+    return false
+  }
 })
 
 export default router
---
+++ src/stores/oauth-store.ts
@@ -0,0 +1,25 @@
+import { ref, type Ref, reactive } from "vue";
+import { defineStore } from "pinia";
+
+import axios, { type AxiosInstance } from "axios";
+
+export const useOauthStore = defineStore("oauth", () => {
+  const isConnected: Ref<boolean> = ref(false);
+
+  const name: Ref<string> = ref("");
+
+  const backend: AxiosInstance = axios.create();
+
+  function $reset() {
+    isConnected.value = false;
+    name.value = "";
+    backend.defaults.headers.common["Authorization"] = "";
+  }
+
+  return {
+    isConnected,
+    backend,
+    name,
+    $reset,
+  };
+});
