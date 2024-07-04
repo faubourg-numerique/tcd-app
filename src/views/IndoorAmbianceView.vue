4b60fb1abcfe337c6b4bba2224f8f92eecac0b87
Ephraim <ephraimgoussanou0@gmail.com>
2024-07-04 15:28:10 +0200

    Refactor view component names for better clarity

--- src/views/IndoorAmbianceView.vue
+++ src/views/IndoorAmbianceView.vue
@@ -1,15 +1,11 @@
-<template>
-    <div>
-        <h1>Ambiance intérieure - LONG</h1>
-        <p>Contenu de la page Ambiance intérieure - LONG.</p>
-    </div>
-</template>
-
-<script lang="ts">
-export default {
-    name: 'IndoorAmbiance'
-};
+<script setup lang="ts">
 </script>
+
+<template>
+  <div>
+    <h1>Indoor Ambiance</h1>
+  </div>
+</template>
 
 <style scoped>
 
--- src/views/WaterLevelView.vue
+++ src/views/WaterLevelView.vue
@@ -1,16 +1,11 @@
-<template>
-    <div>
-        <h1>Niveau d'eau - LONG</h1>
-        <p>Contenu de la page Niveau d'eau - LONG.</p>
-    </div>
-</template>
-
-<script lang="ts">
-export default {
-    name: 'WaterLevel'
-};
+<script setup lang="ts">
 </script>
 
+<template>
+  <div>
+    <h1>Water Level</h1>
+  </div>
+</template>
+
 <style scoped>
-/* Ajoutez vos styles ici */
 </style>
