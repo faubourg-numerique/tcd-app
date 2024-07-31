9906c59f39a8994d4b18292645aeefc370b76da7
Ephraim Goussanou <ephraimgoussanou0@gmail.com>
2024-07-31 14:44:49 +0200

    Add Tcd App Planning  .

---
+++ src/components/Tcd-App-Planning.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+
+
+const calendarOptions = ref({
+  plugins: [dayGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth'
+});
+
+
+</script>
+
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
