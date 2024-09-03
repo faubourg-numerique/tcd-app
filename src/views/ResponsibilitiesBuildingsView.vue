7fb5da551a46096970651c31f12a035256edc818
Pierre Josselin <70814177+pierre-josselin@users.noreply.github.com>
2024-09-03 16:47:36 +0200

    Disable thermostat controls

--- src/views/ResponsibilitiesBuildingsView.vue
+++ src/views/ResponsibilitiesBuildingsView.vue
@@ -27,11 +27,11 @@ async function updateThermostatTemperature(thermostatId: string) {
                     <form class="container border border-danger rounded d-flex flex-column align-items-center h-100 py-3" @submit.prevent="updateThermostatTemperature(thermostat.id)">
                         <div class="mb-3 w-50 form-group">
                             <label for="temperature" class="form-label">Temperature</label>
-                            <input id="temperature" v-model="thermostat.temperature" type="number" min="-20" max="60" step="0.1" placeholder="Temperature" required class="form-control" />
+                            <input id="temperature" v-model="thermostat.temperature" type="number" min="-20" max="60" step="0.1" placeholder="Temperature" required class="form-control" disabled />
                         </div>
                         <div class="mb-3 w-50 text-center form-group">
                             <p class="text-danger">{{ thermostat.name }}</p>
-                            <input type="submit" class="btn btn-primary mt-auto" />
+                            <input type="submit" class="btn btn-primary mt-auto" disabled />
                         </div>
                     </form>
                 </div>
