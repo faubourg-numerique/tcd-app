275318c75878eedd9ea09e9e503fe3489ba791a3
Pierre Josselin <70814177+pierre-josselin@users.noreply.github.com>
2024-09-03 10:04:29 +0200

    Create CityZoneSelector component

---
+++ src/components/CityZoneSelector.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineModel } from "vue";
+
+import { useCityStore } from "@/stores/city-store";
+import { useZoneStore } from "@/stores/zone-store";
+
+const cityStore = useCityStore();
+const zoneStore = useZoneStore();
+
+const selectedCityId = defineModel("selectedCityId");
+const selectedZoneId = defineModel("selectedZoneId");
+</script>
+
+<template>
+    <div class="bg-white p-4 rounded text-center border border-danger">
+        <div class="form-group">
+            <label for="city-id" class="form-label">{{ $t("manager.selectACity") }}</label>
+            <select id="city-id" v-model="selectedCityId" class="form-select">
+                <option :value="null" disabled>{{ $t("manager.selectACity") }}</option>
+                <option v-for="city in cityStore.cities" :key="city.id" :value="city.id">{{ city.name }}</option>
+            </select>
+        </div>
+        <div v-if="selectedCityId" class="form-group">
+            <label for="zone-id" class="form-label">{{ $t("manager.selectAZone") }}</label>
+            <select id="zone-id" v-model="selectedZoneId" class="form-select">
+                <option :value="null" disabled>{{ $t("manager.selectAZone") }}</option>
+                <option v-for="zone in zoneStore.getZonesByCityId(selectedCityId)" :key="zone.id" :value="zone.id">{{ zone.name }}</option>
+            </select>
+        </div>
+    </div>
+</template>
