e5a56d24bcfe621d759b0f95b33b04690136908c
Pierre Josselin <70814177+pierre-josselin@users.noreply.github.com>
2024-09-10 11:28:49 +0200

    Improvements before demo

--- src/views/ExempleDashboardView.vue
+++ src/views/ExempleDashboardView.vue
@@ -5,6 +5,7 @@ const exemplePanel1Url = import.meta.env.VITE_EXEMPLE_DASHBOARD_PANEL_1_URL;
 <template>
     <div class="container">
         <h1 class="mb-4">{{ $t("nav.ExempleDashboard") }}</h1>
+        <small class="d-block mb-3">Qualité de l’air CO2, Température, Humidité, Mouvement /Mairie de LONG / Salle des fêtes</small>
         <div class="row row-cols-1 row-cols-md-2 g-3">
             <div v-for="(panelUrl, index) in [exemplePanel1Url]" :key="index" class="col">
                 <iframe :src="panelUrl" class="w-100" height="300"></iframe>
