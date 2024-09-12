261ff8b73832e52467284250237c430911f33d95
Pierre Josselin <70814177+pierre-josselin@users.noreply.github.com>
2024-09-12 16:34:43 +0200

    Add prettier-plugin-organize-imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import { createPinia } from "pinia";
