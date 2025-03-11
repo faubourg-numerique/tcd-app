042cf214362463345c764e3abeb886fc6e68bdc1
Ephraim Goussanou <ephraimgoussanou0@gmail.com>
2025-03-11 16:25:21 +0100

    Enhance user preferences management: add fetch and update functions, integrate SweetAlert for success notifications

--- src/stores/user-store.ts
+++ src/stores/user-store.ts
@@ -1,12 +1,28 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
 import api from "@/api";
 
-export const useUserStore = defineStore('user', () => {
-    async function getUsers() {
-        const response = await api.get("/users");
-        return response;
+export const useUserStore = defineStore("user", () => {
+    
+    async function fetchUserPreferences(email: string) {
+        try {
+            const response = await api.get(`/users/${email}`);
+            console.log("Préférences récupérées:", response);
+            return response.data; 
+        } catch (error) {
+            console.error("Erreur lors de la récupération des préférences:", error);
+            throw error;
+        }
     }
 
-    return { getUsers };
+    async function updateUserPreferences(email: string, gristApiKey: string, gristDocId: string) {
+        try {
+            const response = await api.put("/users", { email, gristApiKey, gristDocId });
+            return response.data;
+        } catch (error) {
+            console.error("Erreur lors de la mise à jour des préférences:", error);
+            throw error;
+        }
+    }
+
+return { fetchUserPreferences, updateUserPreferences };
 });
