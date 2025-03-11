cad3e93ff4e60695cd2bf333a30ce78d48e0c577
Ephraim Goussanou <ephraimgoussanou0@gmail.com>
2025-03-11 17:08:30 +0100

    Upgrade ...

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/attributes-order -->
 <script setup lang="ts">
 import { useMainStore } from "@/stores/main-store";
 import { useUserStore } from "@/stores/user-store";
@@ -9,13 +10,15 @@ const userStore = useUserStore();
 
 const user = ref({
     gristApiKey: "",
-    gristDocId: ""
+    gristDocId: "", 
+    gristBaseUrl: ""
 });
 
-// Récupérer l'email depuis Keyrock via mainStore
+const preferencesLoaded = ref(false);
+
+
 const userEmail = computed(() => mainStore.user?.email || "Email non disponible");
 
-// Charger les préférences de l'utilisateur
 async function fetchUserPreferences() {
     if (!mainStore.user?.email) return;
 
@@ -24,8 +27,10 @@ async function fetchUserPreferences() {
         if (userData) {
             user.value = {
                 gristApiKey: userData.gristApiKey || "Configurez votre clé API Grist",
-                gristDocId: userData.gristDocId || "Configurez votre ID de document Grist"
+                gristDocId: userData.gristDocId || "Configurez votre ID de document Grist",
+                gristBaseUrl: userData.gristBaseUrl || "Configurez votre URL de base Grist"
             };
+            preferencesLoaded.value = true;
         }
     } catch (error) {
         console.error("Erreur lors de la récupération des préférences:", error);
@@ -37,8 +42,13 @@ async function updatePreferences() {
     if (!mainStore.user?.email) return;
 
     try {
-        await userStore.updateUserPreferences(mainStore.user.email, user.value.gristApiKey, user.value.gristDocId);
-        Swal.fire({title: "Succès",text: "Préférences mises à jour avec succès !",icon: "success",confirmButtonText: "OK"});
+        await userStore.updateUserPreferences(mainStore.user.email, user.value.gristApiKey, user.value.gristDocId , user.value.gristBaseUrl);
+        Swal.fire({
+            title: "Succès",
+            text: "Préférences mises à jour avec succès !",
+            icon: "success",
+            confirmButtonText: "OK"
+        });
     } catch (error) {
         console.error("Erreur lors de la mise à jour des préférences:", error);
     }
@@ -57,23 +67,31 @@ onMounted(() => {
         </div>
     </div>
 
-    <div class="container mt-5">
+    <div v-if="preferencesLoaded" class="container mt-5">
         <h3 class="mb-4">GRIST Préférences</h3>
 
         <form @submit.prevent="updatePreferences">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" id="email" class="form-control" :value="userEmail" disabled />
+                <input id="email" type="email" class="form-control" :value="userEmail" disabled />
             </div>
 
             <div class="mb-3">
                 <label for="gristApiKey" class="form-label">Grist API Key</label>
-                <input type="text" id="gristApiKey" class="form-control" v-model="user.gristApiKey" />
+                <input id="gristApiKey" v-model="user.gristApiKey" type="text" class="form-control" />
             </div>
 
             <div class="mb-3">
                 <label for="gristDocId" class="form-label">Grist Doc ID</label>
-                <input type="text" id="gristDocId" class="form-control" v-model="user.gristDocId" />
+                <input id="gristDocId" v-model="user.gristDocId" type="text" class="form-control" />
+            </div>
+
+            <div class="mb-3">
+                <label for="gristBaseUrl" class="form-label">Grist Base Url</label>
+                <select id="gristBaseUrl" v-model="user.gristBaseUrl" class="form-control">
+                    <option value="https://docs.getgrist.com/api/docs">Grist Standard</option>
+                    <option value="https://grist.incubateur.anct.gouv.fr/api/docs">Grist Global / ANCT</option>
+                </select>
             </div>
 
             <button type="submit" class="btn btn-primary">Enregistrer</button>
