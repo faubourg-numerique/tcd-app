6d67c0dd0eb90b21fdfb1b8178a7e4478f417ad6
Ephraim Goussanou <ephraimgoussanou0@gmail.com>
2025-03-19 16:33:14 +0100

    Add StreetlightMap component to display streetlights on a map and update ResponsibilitiesLightingView to integrate it

--- src/views/ResponsibilitiesLightingView.vue
+++ src/views/ResponsibilitiesLightingView.vue
@@ -3,9 +3,11 @@ import { onMounted, ref, type Ref } from "vue";
 import { useRoute } from "vue-router";
 import { useStreetlightControlCabinetStore } from "@/stores/streetlight-control-cabinet-store";
 import CityZonePicker from "@/components/CityZonePicker.vue";
-import OperationParametersPicker from "@/components/OperationParametersPicker.vue";
-import OperationScheduleCalendar from "@/components/OperationScheduleCalendar.vue";
-import StreetlightControlCabinet from "@/components/StreetlightControlCabinet.vue";
+// import OperationParametersPicker from "@/components/OperationParametersPicker.vue";
+// import OperationScheduleCalendar from "@/components/OperationScheduleCalendar.vue";
+// import StreetlightControlCabinet from "@/components/StreetlightControlCabinet.vue";
+import StreetlightMap from "@/components/StreetlightMap.vue";
+
 
 const route = useRoute();
 const selectedCityId: Ref<string | null> = ref((route.query.cityId as string) ?? null);
@@ -24,10 +26,13 @@ onMounted(() => {
 <template>
     <div class="container">
         <CityZonePicker v-model:selected-city-id="selectedCityId" v-model:selected-zone-id="selectedZoneId" :responsibilities="[]" class="mb-4" />
-        <template v-if="selectedCityId && selectedZoneId">
+        <!-- <template v-if="selectedCityId && selectedZoneId">
             <StreetlightControlCabinet :selected-zone-id="selectedZoneId" />
             <OperationParametersPicker v-model="selectedOperationId" v-model:selected-operation-id="selectedOperationId" v-model:selected-operation-parameters-id="selectedOperationParametersId" :zone-id="selectedZoneId" />
            <OperationScheduleCalendar :city-id="selectedCityId" :zone-id="selectedZoneId" /> 
+        </template> -->
+        <template v-if="selectedCityId && selectedZoneId">
+            <StreetlightMap :selected-zone-id="selectedZoneId" />
         </template>
     </div>
 </template>
